1fafa6344c3f34ec6a8e597662eeb1514020cd58
Fabio <faparedes28@hotmail.com>
2025-05-19 00:17:55 -0500

    cambio de cursor al momento de pasar por los botones

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -16,9 +16,9 @@ function NavBar() {
                     <h1 className="flex justify-center font-sans text-3xl text-white font-bold">BOLETAS YA</h1>
                 </section>
                 <div className="flex flex-col items-start mt-3 gap-2 sm:flex-row smm-0">
-                    <button className="text-2xl text-gray-700 hover:bg-gray-200 px-2 py-2 rounded">Inicio</button>
-                    <button className="text-2xl text-gray-700 hover:bg-gray-200 px-2 py-2 rounded">Información</button>
-                    <button className="text-2xl text-gray-700 hover:bg-gray-200 px-2 py-2 rounded">Contacto</button>
+                    <button className="text-2xl text-gray-700 hover:bg-gray-200 px-2 py-2 rounded cursor-pointer">Inicio</button>
+                    <button className="text-2xl text-gray-700 hover:bg-gray-200 px-2 py-2 rounded cursor-pointer">Información</button>
+                    <button className="text-2xl text-gray-700 hover:bg-gray-200 px-2 py-2 rounded cursor-pointer">Contacto</button>
                     <button className="text-2xl bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-2 rounded-md focus:outline-none focus:shadow-outline transform transition-all hover:scale-105">Iniciar sesion</button>
                 </div>
             </nav>
