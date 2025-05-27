148171e0fa0c7aa7dd38946df48a1d146a0691a9
G4ru75 <dairodu123@gmail.com>
2025-05-26 20:42:51 -0500

    Se agregan los reportes al back

--- src/Components/GenerarReportes.js
+++ src/Components/GenerarReportes.js
@@ -1,20 +1,140 @@
-
-
+import {useState, useEffect} from 'react';
+import Cookies from 'js-cookie';
+import Swal from 'sweetalert2';
 
 function GenerarReporte() {
+
+    const [titulo, setTitulo] = useState('');
+    const [descripcion, setDescripcion] = useState('');
+    const [evento, setEvento] = useState('');
+    const [aforoMaximo, setAforoMaximo] = useState('');
+    const [totalEntradasVendidas, setTotalEntradasVendidas] = useState('');
+    const [totalAsistencias, setTotalAsistencias] = useState('');
+    const [listaEventos, setListaEventos] = useState([]);
+    
+    useEffect(() => {
+            fetch('https://localhost:7047/api/Eventos')
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Error al obtener los eventos');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    const EventosActivos = data.filter(evento => evento.estado == true)
+                    setListaEventos(EventosActivos);
+                    console.log(EventosActivos);
+                })
+                .catch(error => console.error("Error al obtener eventos:", error));
+        }, []);
+
+        useEffect(() => {
+            if(evento){
+                const eventoSeleccionado = listaEventos.find(ev => ev.id_Evento == evento)
+                if(eventoSeleccionado){
+                    setAforoMaximo(eventoSeleccionado.aforo_Max);
+                }
+            }else{
+                setAforoMaximo('');
+            }
+        },[evento, listaEventos]);
+
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    // Convertimos a número por si vienen como strings
+    const aforo = parseInt(aforoMaximo);
+    const entradas = parseInt(totalEntradasVendidas);
+    const asistencias = parseInt(totalAsistencias);
+
+    if (entradas > aforo) {
+        Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Las entradas vendidas no pueden ser mayores al aforo máximo.',
+        });
+        return;
+    }else if(asistencias > entradas){
+        Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Las asistencias no pueden ser mayores a las entradas vendidas.',
+        });
+        return;
+    }
+
+    //metemos a eventoNombre el nombre del evento seleccionado usando una consulta LinQ donde busca el id y guarda el nombre 
+    const eventoNombre = listaEventos.find(ev => ev.id_Evento == evento)?.nombre_Evento || '';
+
+    const Reporte ={
+        nombre_Reporte: titulo,
+        nombre_Evento: eventoNombre,
+        n_ventas: Number(totalEntradasVendidas),
+        n_Asistencias: Number(totalAsistencias),
+        descripcion: descripcion,
+    }; 
+    
+    const token = Cookies.get('token');
+
+    fetch('https://localhost:7047/api/Reportes', {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+
+        body: JSON.stringify(Reporte) 
+    }).then(async (response) => {
+        if (response.ok){
+            Swal.fire({
+                icon: 'success',
+                title: 'Éxito',
+                text: 'Reporte generado correctamente',
+            })
+
+            setTitulo('');
+            setDescripcion('');
+            setEvento('');
+            setAforoMaximo('');
+            setTotalEntradasVendidas('');
+            setTotalAsistencias('');
+
+        }else{
+            const msg = await response.text();
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: msg || 'No se pudo generar el reporte'
+            });
+        }
+    }).catch((error) => {
+        Swal.fire({
+            icon: 'error', 
+            title: 'Error',
+            text: 'Error al generar el reporte'
+        })
+    })
+    
+    
+    }
+
     return (
     <div className="max-w-7xl mx-auto p-6">
         <h1 className="text-3xl font-bold mb-6 text-center">Generar reporte</h1>
 
-        <div className="bg-gray-100 rounded-2xl p-8 shadow-md">
-            <form>
+        <div className="bg-gray-200 rounded-2xl p-8 shadow-md-100">
+            <form onSubmit={handleSubmit} className="space-y-6">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-6">
                 <div>
                 <div className="mb-6">
                     <label htmlFor="tituloReporte" className="block text-xl font-semibold mb-2">
                     Titulo reporte
                     </label>
-                    <input type="text" id="tituloReporte" className="border rounded-md w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"required
+                    <input type="text" id="tituloReporte" className="border rounded-md w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white"required
+                    maxLength={100}
+                    placeholder="Ingrese el título del reporte"
+                    value={titulo}
+                    onChange={(e) => setTitulo(e.target.value)}
                     />
                 </div>
 
@@ -22,7 +142,12 @@ function GenerarReporte() {
                     <label htmlFor="descripcion" className="block text-xl font-semibold mb-2">
                     Descripción
                     </label>
-                    <textarea id="descripcion" className="w-full h-48 bg-white p-3 focus:outline-none focus:ring-2 focus:ring-blue-500"required/>
+                    <textarea id="descripcion" className="w-full h-48 bg-white p-3 focus:outline-none focus:ring-2 focus:ring-blue-500"required
+                    placeholder='Ingrese una descripción del reporte'
+                    maxLength={500}
+                    value={descripcion}
+                    onChange={(e) => setDescripcion(e.target.value)}
+                    />
                 </div>
             </div>
 
@@ -32,23 +157,16 @@ function GenerarReporte() {
                     <label htmlFor="evento" className="block text-xl font-semibold mb-2">
                     evento
                     </label>
-                    <select id="evento" className="border rounded-md w-full py-2 px-3 text-gray-700  focus:outline-none focus:ring-2 focus:ring-blue-500"required>
+                    <select id="evento" className="border rounded-md w-full py-2 px-3 text-gray-700  focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white"required
+                    value={evento}
+                    onChange={(e) => setEvento(e.target.value)}
+                    >
                         <option value="">Seleccione</option>
-                        <option value="1">Concierto Rock</option>
-                        <option value="2">Festival de Jazz</option>
-                        <option value="3">Obra de Teatro</option>
-                        <option value="4">Evento Deportivo</option>
-                    </select>
-                </div>
-
-                <div className="mb-6">
-                    <label htmlFor="idEvento" className="block text-xl font-semibold mb-2">
-                    Id evento
-                    </label>
-                    <select id="idEvento" className="border rounded-md w-full py-2 px-3 text-gray-700  focus:outline-none focus:ring-2 focus:ring-blue-500"required>
-                        <option value="A001">001 </option>
-                        <option value="A002">002 </option>
-                        <option value="A003">003 </option>
+                        {listaEventos.map((ev) => (
+                            <option key={ev.id_Evento} value={ev.id_Evento}>
+                                {ev.nombre_Evento}
+                            </option>
+                        ))}
                     </select>
                 </div>
 
@@ -56,21 +174,30 @@ function GenerarReporte() {
                     <label htmlFor="aforoMaximo" className="block text-xl font-semibold mb-2">
                     Aforo maximo
                     </label>
-                    <input type="text" id="aforoMaximo" className="border rounded-md w-full py-2 px-3 text-gray-700  focus:outline-none focus:ring-2 focus:ring-blue-500"readOnly/>
+                    <input type="number" id="aforoMaximo" className="border rounded-md w-full py-2 px-3 text-gray-700  focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white" readOnly
+                    value={aforoMaximo}
+                    onChange={(e) => setAforoMaximo(e.target.value)}
+                    />
                 </div>
 
                 <div className="mb-6">
                     <label htmlFor="totalEntradasVendidas" className="block text-xl font-semibold mb-2">
                     Total entradas vendidas
                     </label>
-                    <input type="text" id="totalEntradasVendidas" className="border rounded-md w-full py-2 px-3 text-gray-700  focus:outline-none focus:ring-2 focus:ring-blue-500"readOnly/>
+                    <input type="number" id="totalEntradasVendidas" className="border rounded-md w-full py-2 px-3 text-gray-700  focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white"required
+                    value={totalEntradasVendidas}
+                    onChange={(e) => setTotalEntradasVendidas(e.target.value)}
+                    />
                 </div>
 
                 <div className="mb-6">
                     <label htmlFor="totalAsistencias" className="block text-xl font-semibold mb-2">
                     Total asistencias
                     </label>
-                    <input type="text" id="totalAsistencias" className="border rounded-md w-full py-2 px-3 text-gray-700  focus:outline-none focus:ring-2 focus:ring-blue-500"readOnly/>
+                    <input type="number" id="totalAsistencias" className="border rounded-md w-full py-2 px-3 text-gray-700  focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white"required
+                    value={totalAsistencias}
+                    onChange={(e) => setTotalAsistencias(e.target.value)}
+                    />
                 </div>
             </div>
             </div>
